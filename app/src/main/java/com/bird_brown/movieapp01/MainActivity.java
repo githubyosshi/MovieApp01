0d035bb34b9f17c4bfba34189842a242d5cd686e
githubyosshi <kanrisya.shisutemu@gmail.com>
2020-02-11 12:51:27 +0900

    Function implementation 4 commit

--- app/src/main/java/com/bird_brown/movieapp01/MainActivity.java
+++ app/src/main/java/com/bird_brown/movieapp01/MainActivity.java
@@ -1,6 +1,9 @@
 package com.bird_brown.movieapp01;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //動画ファイルの場所を設定
+        String uri = "android.resource://" + getPackageName() + "/" + R.raw.movie;
+
+        //VideoViewオブジェクトを取得
+        VideoView video =(VideoView)findViewById(R.id.videoView1);
+
+        //VideoViewにフォーカスをあてる
+        video.requestFocus();
+
+        //VideoViewにMediaControllerを設定（再生、停止、一時停止のボタン表示される）
+        video.setMediaController(new MediaController(this));
+
+        //動画ファイルを読込設定
+        video.setVideoURI(Uri.parse(uri));
+
+        //動画再生
+        video.start();
+
     }
 }
